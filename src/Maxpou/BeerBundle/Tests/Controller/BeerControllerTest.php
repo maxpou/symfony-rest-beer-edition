0987467d6cb2c13d5d3a7a377bd99585e63a8bca
Maxence POUTORD <maxence.poutord@gmail.com>
2016-03-16 12:19:26 +0100

    Update README + add new tests (functionals tests in back office)

--- src/Maxpou/BeerBundle/Tests/Controller/BeerControllerTest.php
+++ src/Maxpou/BeerBundle/Tests/Controller/BeerControllerTest.php
@@ -2,54 +2,83 @@
 
 namespace Maxpou\BeerBundle\Tests\Controller;
 
+use Maxpou\BeerBundle\DataFixtures\ORM\LoadBeersData;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Loader;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 
 class BeerControllerTest extends WebTestCase
 {
-    /*
+    public static function setUpBeforeClass()
+    {
+        $client = static::createClient();
+        $em = $client->getContainer()
+                     ->get('doctrine')
+                     ->getManager();
+
+        $loader = new Loader();
+        $loader->addFixture(new LoadBeersData());
+
+        $purger = new ORMPurger();
+        $executor = new ORMExecutor($em, $purger);
+        $executor->execute($loader->getFixtures());
+    }
+
     public function testCompleteScenario()
     {
         // Create a new client to browse the application
         $client = static::createClient();
 
         // Create a new entry in the database
-        $crawler = $client->request('GET', '/beer/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /beer/");
+        $crawler = $client->request('GET', '/admin/beer/');
+        $this->assertEquals(
+            200,
+            $client->getResponse()->getStatusCode(),
+            "Unexpected HTTP status code for GET /admin/beer/"
+        );
         $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
 
         // Fill in the form and submit it
         $form = $crawler->selectButton('Create')->form(array(
-            'maxpou_beerbundle_beer[field_name]'  => 'Test',
-            // ... other fields to fill
+            'name'        => 'TestBeer',
+            'description' => 'TestDescription',
+            'alcohol'     => 9.3
+            //default Brewery = first one
         ));
 
         $client->submit($form);
         $crawler = $client->followRedirect();
 
         // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('td:contains("TestBeer")')->count(),
+            'Missing element td:contains("TestBeer")'
+        );
 
         // Edit the entity
         $crawler = $client->click($crawler->selectLink('Edit')->link());
 
-        $form = $crawler->selectButton('Update')->form(array(
-            'maxpou_beerbundle_beer[field_name]'  => 'Foo',
-            // ... other fields to fill
+        $form = $crawler->selectButton('Edit')->form(array(
+            'name'  => 'FooBeer'
         ));
 
         $client->submit($form);
         $crawler = $client->followRedirect();
 
         // Check the element contains an attribute with value equals "Foo"
-        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('[value="FooBeer"]')->count(),
+            'Missing element [value="FooBeer"]'
+        );
 
         // Delete the entity
         $client->submit($crawler->selectButton('Delete')->form());
         $crawler = $client->followRedirect();
 
         // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
+        $this->assertNotRegExp('/FooBeer/', $client->getResponse()->getContent());
     }
-
-    */
 }
--- src/Maxpou/BeerBundle/Tests/Controller/BreweryControllerTest.php
+++ src/Maxpou/BeerBundle/Tests/Controller/BreweryControllerTest.php
@@ -6,50 +6,54 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BreweryControllerTest extends WebTestCase
 {
-    /*
     public function testCompleteScenario()
     {
         // Create a new client to browse the application
         $client = static::createClient();
 
         // Create a new entry in the database
-        $crawler = $client->request('GET', '/brewery/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /brewery/");
+        $crawler = $client->request('GET', '/admin/brewery/');
+        $this->assertEquals(
+            200,
+            $client->getResponse()->getStatusCode(),
+            "Unexpected HTTP status code for GET /admin/brewery/"
+        );
         $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
 
         // Fill in the form and submit it
         $form = $crawler->selectButton('Create')->form(array(
-            'maxpou_beerbundle_brewery[field_name]'  => 'Test',
-            // ... other fields to fill
+            'name'        => 'TestBrewery',
+            'description' => 'TestDescription',
         ));
 
         $client->submit($form);
         $crawler = $client->followRedirect();
 
         // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('td:contains("Test")')->count(),
+            'Missing element td:contains("Test")'
+        );
 
         // Edit the entity
         $crawler = $client->click($crawler->selectLink('Edit')->link());
 
-        $form = $crawler->selectButton('Update')->form(array(
-            'maxpou_beerbundle_brewery[field_name]'  => 'Foo',
-            // ... other fields to fill
+        $form = $crawler->selectButton('Edit')->form(array(
+            'name'  => 'FooBrewery',
         ));
 
         $client->submit($form);
         $crawler = $client->followRedirect();
 
         // Check the element contains an attribute with value equals "Foo"
-        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
+        $this->assertGreaterThan(0, $crawler->filter('[value="FooBrewery"]')->count(), 'Missing element [value="Foo"]');
 
         // Delete the entity
         $client->submit($crawler->selectButton('Delete')->form());
         $crawler = $client->followRedirect();
 
         // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
+        $this->assertNotRegExp('/FooBrewery/', $client->getResponse()->getContent());
     }
-
-    */
 }
